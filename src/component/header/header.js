83f2b3e3574cc330d8be77bf685016c2cc1bef20
Pavel Smolov <palexsmol@gmail.com>
2019-03-11 14:13:39 -0700

    first commit

---
+++ src/component/header/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.css'
+
+const Header = () => {
+    return (
+        <div className="app-header d-flex"></div>
+    );
+};
+
+export default Header;
---
+++ src/services/swapi-service.js
@@ -0,0 +1,46 @@
+
+export default class SwapiService{
+    _apiBase = 'https://swapi.co/api';
+
+    async getResource(url){
+        const res = await fetch(`${this._apiBase}${url}`);
+        
+        if (!res.ok){
+            throw new Error(`Could not fetch ${url}`+
+            `, recieved ${res.status}`)
+        }
+        return await res.json();
+    }
+
+    async getAllPeople(){
+        const res = await this.getResource(`/people/`);
+        return res.results;
+    }
+    getAllPerson(id){
+        return this.getResource(`/people/${id}/`);
+    }
+
+    async getAllPlanets(){
+        const res = await this.getResource(`/planets/`);
+        return res.results;
+    }
+    getAllPlanet(id){
+        return this.getResource(`/planets/${id}/`);
+    }
+
+    async getAllStarships(){
+        const res = await this.getResource(`/starships/`);
+        return res.results;
+    }
+    getAllStarship(id){
+        return this.getResource(`/starships/${id}/`);
+    }
+}
+
+const swapi = new SwapiService();
+swapi.getAllPeople().then((people) => {
+    people.forEach((p) => {
+        console.log(p.name);
+    })
+});
+

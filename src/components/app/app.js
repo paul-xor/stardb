18e124cea05851521151599df72c6f9e0308196a
Pasha Smolov <palexsmol@gmail.com>
2019-09-05 18:40:05 -0700

    context api

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,9 @@ import ErrorBoundry from "../error-boundry";
 
 import ItemDetails, { Record } from "../item-details/item-details";
 import SwapiService from "../../services/swapi-service";
+import DummySwapiService from "../../services/dummy-swapi-service";
+
+import { SwapiServiceProvider } from "../swapi-service-context";
 
 import {
   PersonDetails,
@@ -19,7 +22,7 @@ import {
 import "./app.css";
 
 export default class App extends Component {
-  swapiService = new SwapiService();
+  swapiService = new DummySwapiService();
 
   state = {
     showRandomPlanet: true
@@ -66,21 +69,23 @@ export default class App extends Component {
 
     return (
       <ErrorBoundry>
-        <div className="stardb-app">
-          <Header />
+        <SwapiServiceProvider value={this.swapiService}>
+          <div className="stardb-app">
+            <Header />
 
-          <PersonDetails itemId={11} />
+            <PersonDetails itemId={11} />
 
-          <PlanetDetails itemId={5} />
+            <PlanetDetails itemId={5} />
 
-          <StarshipDetails itemId={9} />
+            <StarshipDetails itemId={9} />
 
-          <PersonList />
+            <PersonList />
 
-          <StarshipList />
+            <StarshipList />
 
-          <PlanetList />
-        </div>
+            <PlanetList />
+          </div>
+        </SwapiServiceProvider>
       </ErrorBoundry>
     );
   }

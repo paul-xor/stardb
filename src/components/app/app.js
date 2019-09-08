ee4b70b53048528bcf5e90455ba0003febdf3b30
Pasha Smolov <palexsmol@gmail.com>
2019-09-08 15:50:19 -0700

    add router-dom and nav links

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -12,6 +12,8 @@ import { SwapiServiceProvider } from "../swapi-service-context";
 
 import "./app.css";
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 export default class App extends Component {
   state = {
     showRandomPlanet: true,
@@ -34,13 +36,16 @@ export default class App extends Component {
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.state.swapiService}>
-          <div className="stardb-app">
-            <Header onServiceChange={this.onServiceChange} />
-            <RandomPlanet />
-            <PeoplePage />
-            <PlanetsPage />
-            <StarshipsPage />
-          </div>
+          <Router>
+            <div className="stardb-app">
+              <Header onServiceChange={this.onServiceChange} />
+              <RandomPlanet />
+
+              <Route path="/people" component={PeoplePage} />
+              <Route path="/planets" component={PlanetsPage} />
+              <Route path="/starships" component={StarshipsPage} />
+            </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     );

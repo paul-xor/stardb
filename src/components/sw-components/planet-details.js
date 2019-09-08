1dc7193ae269ea5b07943a7df173923b4c9a3120
Pasha Smolov <palexsmol@gmail.com>
2019-09-08 11:36:39 -0700

    refactoring of HOC

--- src/components/sw-components/planet-details.js
+++ src/components/sw-components/planet-details.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ItemDetails, { Record } from '../item-details';
-import { withSwapiService } from '../hoc-helpers';
+import React from "react";
+import ItemDetails, { Record } from "../item-details";
+import { withSwapiService } from "../hoc-helpers";
 
-const PlanetDetails = (props) => {
+const PlanetDetails = props => {
   return (
     <ItemDetails {...props}>
       <Record field="population" label="Population" />
@@ -12,11 +12,11 @@ const PlanetDetails = (props) => {
   );
 };
 
-const mapMethodsToProps = (swapiService) => {
+const mapMethodsToProps = swapiService => {
   return {
     getData: swapiService.getPlanet,
     getImageUrl: swapiService.getPlanetImage
   };
 };
 
-export default withSwapiService(PlanetDetails, mapMethodsToProps);
+export default withSwapiService(mapMethodsToProps)(PlanetDetails);
--- src/components/sw-components/starship-details.js
+++ src/components/sw-components/starship-details.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ItemDetails, { Record } from '../item-details';
-import { withSwapiService } from '../hoc-helpers';
+import React from "react";
+import ItemDetails, { Record } from "../item-details";
+import { withSwapiService } from "../hoc-helpers";
 
-const StarshipDetails = (props) => {
+const StarshipDetails = props => {
   return (
     <ItemDetails {...props}>
       <Record field="model" label="Model" />
@@ -12,11 +12,11 @@ const StarshipDetails = (props) => {
   );
 };
 
-const mapMethodsToProps = (swapiService) => {
+const mapMethodsToProps = swapiService => {
   return {
     getData: swapiService.getStarship,
     getImageUrl: swapiService.getStarshipImage
-  }
+  };
 };
 
-export default withSwapiService(StarshipDetails, mapMethodsToProps);
+export default withSwapiService(mapMethodsToProps)(StarshipDetails);
